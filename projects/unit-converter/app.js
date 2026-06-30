90d618ea3f4aed8bc2b7f0fb83aabea3bd48e9b8
zickOne <zickstep@gmail.com>
2026-06-29 22:35:15 -0600

    Feat: add fahrenheit to celsius conversion logic

--- projects/unit-converter/app.js
+++ projects/unit-converter/app.js
@@ -12,3 +12,14 @@ function convertCelsiusToFahrenheit(celsius) {
 // Example usage:
 const tempCelsius = 25;
 console.log(`${tempCelsius}°C is ${convertCelsiusToFahrenheit(tempCelsius)}°F`);
+
+/**
+ * @description Converts a temperature from Fahrenheit to Celsius.
+ * @author Javier Ibanez
+ * @date 2026-06-29
+ * @param {number} fahrenheit - The temperature in degrees Fahrenheit.
+ * @returns {number} - The temperature in degrees Celsius.
+ */
+function convertFahrenheitToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
